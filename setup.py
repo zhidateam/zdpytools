1b7c514b2f74de9e879af7c062d12b5e06911020
xy3 <120182408@qq.com>
2025-04-26 17:39:48 +0800

    打包新版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="zdpytools",
-    version="0.1.22",
+    version="0.1.23",
     author="zhidateam",
     author_email="zhidateam@163.com",
     description="Python工具集，包含飞书API、AutoDL API和日志工具",
